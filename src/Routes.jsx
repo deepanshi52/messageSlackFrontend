048ab795dd60646851278a814e91cd9a3f867091
deepanshi sharma <deepanshis013@gmail.com>
2025-05-27 16:50:50 +0530

    Added protected routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,13 +4,14 @@ import { SignupContainer } from "@/components/organisms/Auth/SignupContainer"
 import { NotFound } from "@/pages/Auth/Notfound/Notfound"
 import { SigninContainer } from "@/components/organisms/Auth/SigninContainer"
 import { ProtectedRoute } from "./components/molecules/ProtectedRoute/ProtectedRoute"
+import { Home } from "@/pages/Home/Home"
 
 export const AppRoutes = () => {
     return(
          <Routes>
         <Route path="/auth/signup" element={<Auth><SignupContainer /></Auth>} />
         <Route path="/auth/signin" element={<Auth><SigninContainer /></Auth>} />
-        <Route path="/home" element={<ProtectedRoute><Auth><h1>Home</h1></Auth></ProtectedRoute>} />
+        <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     )
